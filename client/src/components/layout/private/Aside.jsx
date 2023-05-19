9db9aefb13571148055b2c0b29949975bc5f907d
Harold-GA <haroldgutierrezhga@gmail.com>
2023-05-19 16:38:59 +0200

    Function save publication done

--- client/src/components/layout/private/Aside.jsx
+++ client/src/components/layout/private/Aside.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useAuth from '../../../hooks/useAuth'
 import { Avatar } from './Avatar'
 import { Link } from 'react-router-dom'
-
+import { Global } from '../../../helpers/Global'
+import { useForm } from '../../../hooks/useForm'
 export const Aside = () => {
-  const { auth, counters } = useAuth()
+  const { auth, counters, setCounters } = useAuth()
+  const { form, changed } = useForm()
+  const [saved, setSaved] = useState('not saved')
+
+  const Save = e => {
+    e.preventDefault()
+    const newPublication = form
+    const token = localStorage.getItem('token')
+    fetch(`${Global.url}/publication/save`, {
+      method: 'POST',
+      body: JSON.stringify(newPublication),
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: token,
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.status == 'Success') {
+          setCounters({ ...counters, publications: counters.publications + 1 })
+          setSaved('Saved')
+        } else {
+          setSaved('Error')
+        }
+
+        //Upload image
+        const fileInput = document.querySelector('#file')
+        if (data.status == 'Success' && fileInput.files[0]) {
+          const formData = new FormData()
+          formData.append('file0', fileInput.files[0])
+          fetch(`${Global.url}/publication/upload/${data.publication._id}`, {
+            method: 'POST',
+            body: formData,
+            headers: {
+              Authorization: token,
+            },
+          })
+            .then(res => res.json())
+            .then(uploadData => {
+              if (uploadData.status == 'Success') {
+                setSaved('Saved')
+              } else {
+                setSaved('Error')
+              }
+              if (data.status == 'Success' && uploadData.status == 'Success') {
+                const myForm = document.querySelector('#publication-form')
+                myForm.reset()
+              }
+            })
+        }
+      })
+  }
 
   return (
     <aside className='layout__aside'>
@@ -51,22 +103,27 @@ export const Aside = () => {
         </div>
 
         <div className='aside__container-form'>
-          <form className='container-form__form-post'>
+          {saved == 'Saved' ? (
+            <strong className='alert alert_success'>Publication upload!!</strong>
+          ) : (
+            ''
+          )}
+          <form className='container-form__form-post' onSubmit={Save} id='publication-form'>
             <div className='form-post__inputs'>
-              <label htmlFor='post' className='form-post__label'>
+              <label htmlFor='text' className='form-post__label'>
                 ¿What are you thinking today?
               </label>
-              <textarea name='post' className='form-post__textarea'></textarea>
+              <textarea name='text' className='form-post__textarea' onChange={changed} />
             </div>
 
             <div className='form-post__inputs'>
-              <label htmlFor='image' className='form-post__label'>
+              <label htmlFor='file' className='form-post__label' onChange={changed}>
                 Upload image
               </label>
-              <input type='file' name='image' className='form-post__image' />
+              <input type='file' name='file0' id='file' className='form-post__image' />
             </div>
 
-            <input type='submit' value='Send' className='form-post__btn-submit' disabled />
+            <input type='submit' value='Send' className='form-post__btn-submit' />
           </form>
         </div>
       </div>
